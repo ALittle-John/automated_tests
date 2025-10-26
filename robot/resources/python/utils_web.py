3f6823f56790a8a8361493cec845412e1e2003fd
ALittle-John <joaopsousaqi@gmail.com>
2025-10-26 20:10:28 -0300

    feature: added webdriver-manager to oben brwser

--- robot/resources/python/utils_web.py
+++ robot/resources/python/utils_web.py
@@ -1,4 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.edge.service import Service as EdgeService
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+
 from selenium.webdriver.common.by import By
 
 locator_map = {
@@ -14,39 +21,44 @@ locator_map = {
 
 class SetupBrowser:
     # Possible non-use in robot resources
-    def open_browser(self, browser_name: str):
+    @staticmethod
+    def open_browser(browser_name: str):
         browser_name = browser_name.lower()
         if browser_name == "chrome":
-            browser = webdriver.Chrome()
+            browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
         elif browser_name == "firefox":
-            browser = webdriver.Firefox()
+            browser = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
         elif browser_name == "edge":
-            browser = webdriver.Edge()
+            browser = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
         else:
             raise ValueError(f"Unsupported browser: {browser_name}")
         browser.maximize_window()
 
         return browser
 
-    def shut_down_browser(self, browser: webdriver):
+    @staticmethod
+    def shut_down_browser(browser: webdriver):
         browser.quit()
 
-    def search_website(self, browser: webdriver, url: str):
+    @staticmethod
+    def search_website(browser: webdriver, url: str):
         browser.get(url)
 
 class WebElementInteractions:
     # Possible non-use of most functions in robot resources
-    def prove_element_present(self, browser: webdriver, locator_type: str, locator_value: str):
+    @staticmethod
+    def prove_element_present(browser: webdriver, locator_type: str, locator_value: str):
         locator_type_upper = locator_type.upper()
         web_element = browser.find_element(locator_map[locator_type_upper], locator_value)
 
         return web_element
 
-    def prove_elements_present(self, browser: webdriver, locator_type: str, locator_value: str):
+    @staticmethod
+    def prove_elements_present(browser: webdriver, locator_type: str, locator_value: str):
         locator_type_upper = locator_type.upper()
         web_elements = browser.find_elements(locator_map[locator_type_upper], locator_value)
 
         return web_elements
 
-    # def search_text(self, browser: webdriver, web_element: object, text: any):
+    # def search_text(browser: webdriver, web_element: object, text: any):
 
